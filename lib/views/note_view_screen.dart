030d1beae8001e4bf7a49acd9fa291168d19f963
bob <bob@>
2024-12-06 11:09:20 +0200

    create text field

--- lib/views/note_view_screen.dart
+++ lib/views/note_view_screen.dart
@@ -9,7 +9,12 @@ class NoteViewScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(onPressed: () {
-        showModalBottomSheet(context: context, builder: (context) {
+        showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24)
+          ),
+            context: context, 
+            builder: (context) {
           return const AddNoteBottomSheet();
         });
       },
--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/constants.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          const SizedBox(height: 32),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Title',
+              hintStyle: const TextStyle(color: kPrimaryColor),
+              border: buildBorder(),
+              enabledBorder: buildBorder(),
+              focusedBorder: buildBorder(kPrimaryColor)
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder ([color]) {
+    return OutlineInputBorder(
+            borderRadius: BorderRadius.circular(24),
+            borderSide: BorderSide(
+                color: color ?? Colors.white),
+          );
   }
 }
