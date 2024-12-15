23005d8627edfa0079bf7e6569c1dd1d330a141e
bob <bob@>
2024-12-15 16:06:45 +0200

    add button to bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/constants.dart';
 import 'package:note_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -6,15 +7,36 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextField(hintName: 'Title'),
-          SizedBox(height: 18,),
-          CustomTextField(hintName: 'Content', maxLines: 5,),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 32),
+            const CustomTextField(hintName: 'Title'),
+            const SizedBox(height: 18,),
+            const CustomTextField(hintName: 'Content', maxLines: 5,),
+            const SizedBox(height: 62,),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 52,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(1)
+              ),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: kPrimaryColor
+                  ),
+                  child: const Text('Add', style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black
+                  ),)),
+            ),
+            const SizedBox(height: 28,),
+          ],
+        ),
       ),
     );
   }
